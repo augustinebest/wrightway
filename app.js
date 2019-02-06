f422899f9724b9f97c385c01896bc11cc5d1d71d
Igwe Augustine <augustusbest00@gmail.com>
2019-02-06 08:31:49 -0900

    'nice'

--- app.js
+++ app.js
@@ -37,16 +37,13 @@ app.use(bodyparser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(bodyparser.json());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(__dirname + '/client'));
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(__dirname+'/client/public'));
 
-// Production MOde
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + 'client/build/index.html'))
-    })
-}
-
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 app.get('/', function(req, res) {
     res.json({message: 'This is my backend!'});
