71a420215f7ddcbd7a90b74456118db490c9847f
Igwe Augustine <augustusbest00@gmail.com>
2019-02-07 08:16:34 -0900

    'nice'

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ if(process.env.NODE_ENV === 'production') {
     // Serve all static files
     app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
     })
 }
 
