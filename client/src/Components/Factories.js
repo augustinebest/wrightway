19d90d4a9e425e7613be7dae776d2731881dc1ca
Igwe Augustine <augustusbest00@gmail.com>
2019-02-07 07:01:46 -0900

    'nice'

--- client/src/Components/Factories.js
+++ client/src/Components/Factories.js
@@ -1,4 +1,4 @@
 module.exports = {
-    Url: 'http://localhost:4000',
-    // Url: 'https://wrightway.herokuapp.com'
+    // Url: 'http://localhost:4000',
+    Url: 'https://wrightway.herokuapp.com'
 }
