a4964ae37dc60c08f8c8926c8a8b9760001fa825
Igwe Augustine <augustusbest00@gmail.com>
2019-02-07 09:55:55 -0900

    'nice'

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ app.use(bodyparser.json());
 
 if(process.env.NODE_ENV === 'production') {
     // Serve all static files
-    app.use(express.static(path.resolve(__dirname, '/client/build')));  
+    app.use(express.static(path.join(__dirname, 'client/build')));  
     // Handle React routing, return all requests to React app
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
+        res.sendFile(path.join(__dirname, '/client/build/index.html'));
     })
 }
 
